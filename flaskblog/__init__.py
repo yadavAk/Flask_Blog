3767055ccce91ada93eb3ed8c4669fdd14261436
Abhishek Yadav <abhi.cs.0217@gmail.com>
2019-11-16 11:13:53 +0530

    missing files are uploaded and unncessary files are removed

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 # export FLASK_APP=flaskblog.py
 # export FLASK_DEBUG=1
@@ -10,5 +11,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5ae7a2f19377000f6c12d91c84923d46'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
