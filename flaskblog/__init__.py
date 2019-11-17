62ec6a8b91998f9e5d0ebbb1fd7abba430a7b262
Abhishek Yadav <abhi.cs.0217@gmail.com>
2019-11-18 02:36:35 +0530

    Email verification and Password reset

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,8 +1,10 @@
+import os
 from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 # export FLASK_APP=flaskblog.py
 # export FLASK_DEBUG=1
@@ -16,5 +18,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
 
 from flaskblog import routes
